c4ee1431a28b0c2d23c16b2d639e10628efe13c7
Eric Rocha <ericrochaeric@gmail.com>
2021-06-23 02:49:09 -0300

    aula02 inicio

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { 
   Image,
   View,
-  Text,
+  Text
 } from 'react-native'
-import { StatusBar } from 'expo-status-bar';
+
 
 import IllustrationImg from '../../assets/illustration.png'
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -15,16 +15,15 @@ export function SignIn() {
 
   return (
     <View style={styles.container}>
-      <StatusBar style='light' />
       <Image source={IllustrationImg} style={styles.image} resizeMode='stretch' />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Conecte-se {`\n`}
-          e organize suas {`\n`}
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
           jogatinas
         </Text>
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
         <ButtonIcon activeOpacity={0.7} title="Entrar com Discord" />
